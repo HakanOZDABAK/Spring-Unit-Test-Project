511e4244879b34338b9a4e83d375dd7268bc0482
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2023-12-27 13:15:26 +0300

    Added New tests

--- src/test/java/com/hakanozdabak/rentACar/service/BrandServiceTest.java
+++ src/test/java/com/hakanozdabak/rentACar/service/BrandServiceTest.java
@@ -5,6 +5,7 @@ import com.hakanozdabak.rentACar.business.abstracts.BrandService;
 import com.hakanozdabak.rentACar.business.concretes.BrandManager;
 import com.hakanozdabak.rentACar.dataAccess.abstracts.BrandRepository;
 import com.hakanozdabak.rentACar.entities.concretes.Brand;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,9 +14,14 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class BrandServiceTest {
@@ -52,10 +58,43 @@ public class BrandServiceTest {
                 .brandId(1)
                 .brandName("Mercedes")
                 .build();
-        Mockito.when(brandRepository.save(Mockito.any(Brand.class))).thenReturn(brand);
-        brandManager.add(brand);
-        this.brandManager.delete(brand.getBrandId());
-
-       assertThat(brandRepository.count()).isEqualTo(0);
+        assertAll(()->brandManager.delete(brand.getBrandId()));
     }
+
+    @Test
+    @DisplayName("Should Update Brand")
+    public void shouldUpdateBrand() {
+
+
+        Brand brandToUpdate = new Brand();
+        when(brandRepository.save(brandToUpdate)).thenReturn(brandToUpdate);
+
+
+        Brand updatedBrand = brandManager.update(brandToUpdate);
+
+
+        verify(brandRepository).save(brandToUpdate);
+        assertEquals(brandToUpdate, updatedBrand);
+    }
+
+    @Test
+    @DisplayName("Should Get Brands List")
+    public void shouldGetBrandsList() {
+
+
+        List<Brand> mockBrandList = Arrays.asList(
+                new Brand(1, "Mercedes"),
+                new Brand(2, "BMW")
+        );
+        when(brandRepository.findAll()).thenReturn(mockBrandList);
+
+
+        List<Brand> resultBrandList = brandManager.getAll();
+        System.out.println(resultBrandList);
+
+
+        verify(brandRepository).findAll();
+        assertEquals(mockBrandList, resultBrandList);
+    }
+
 }
